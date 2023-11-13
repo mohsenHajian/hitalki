c8a42b489ead00290283abef8302f24ac32a2629
mohsenHajian <m.hajian3451@gmail.com>
2023-11-14 01:46:19 +0330

    time table ui is completed

---
+++ src/views/TimeTable.vue
@@ -0,0 +1,25 @@
+<script>
+import { inject } from 'vue'
+import DayColumn from '../components/DayColumn.vue'
+export default {
+  setup() {
+    const moment = inject('moment')
+    return { moment }
+  },
+  components: {
+    DayColumn
+  }
+}
+</script>
+
+<template>
+  <div class="time-table-page">
+    <DayColumn />
+    <DayColumn />
+    <DayColumn />
+    <DayColumn />
+    <DayColumn />
+    <DayColumn />
+    <DayColumn />
+  </div>
+</template>
